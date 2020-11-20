8f6060bab86b77eea07c05bcc3473bbe0671093a
Kanykei02 <almazbekovakany@gmail.com>
2020-11-20 16:42:52 +0600

    home work with class work

---
+++ src/com/company/Students.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Students {
+    String skinColor;
+    int year;
+    int growth;
+    String nationality;
+
+}
---
+++ src/com/company/Supernatural.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Supernatural {
+    String status;
+    int year;
+    int growth;
+    String power;
+}
