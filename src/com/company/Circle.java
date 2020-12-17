385ecb1932fa97612b619830dd10590d0f48f81e
Kanykei02 <almazbekovakany@gmail.com>
2020-12-17 12:39:16 +0600

    Zadacha1

---
+++ src/com/company/Circle.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Random;
+
+public class Circle {
+    private double radius;
+
+    public Circle(){}
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public  double getCircleArea() {
+        return Math.PI * getRadius() * 2;
+    }
+
+    public static Circle getGenerateCircle() {
+        Random rand = new Random();
+        int randomRadius = rand.nextInt(11);
+        return new Circle(randomRadius);
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Circle[] circle = new Circle[10];
+        int count = 0;
+        for (int i = 0; i < circle.length; i++){
+            circle[i] = Circle.getGenerateCircle();
+            if (i % 3 == 0) {
+                count++;
+                System.out.print(i + " ");
+            }
+        }
+        System.out.printf("\n%s", count);
     }
 }
