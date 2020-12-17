fb92a10cc676731827909255350150a9ebe674e0
Kanykei02 <almazbekovakany@gmail.com>
2020-12-17 13:51:15 +0600

    Zadacha2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,34 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Pizza pizza = new Pizza();
+        pizza.setPrice(300);
+        pizza.setWeight(2);
+        pizza.cooking();
+        pizza.delivery();
+        System.out.println();
+
+        Pepperoni pepperoni = new Pepperoni();
+        pepperoni.setPrice(250);
+        pepperoni.setWeight(1.5);
+        pepperoni.setSpicy(0);
+        pepperoni.cooking();
+        pepperoni.delivery();
+        System.out.println();
+
+        Margarita margarita = new Margarita();
+        margarita.setPrice(280);
+        margarita.setWeight(2.5);
+        margarita.setMeat(1);
+        margarita.cooking();
+        margarita.delivery();
+        System.out.println();
+
+        LaFinta laFinta= new LaFinta();
+        laFinta.setPrice(200);
+        laFinta.setWeight(2.1);
+        laFinta.setMushrooms(1);
+        laFinta.cooking();
+        laFinta.delivery();
     }
 }
