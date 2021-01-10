47dbdbe843ec2ca4ba3859c5c334dec7af0ab1a7
Kanykei02 <almazbekovakany@gmail.com>
2021-01-10 18:50:53 +0600

    Lessons26

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        final int[] array = new int[10];
+        for(int i = 0; i < array.length; i++){
+            array[i] = i;
+            array[i] = i + 5;
+            System.out.print(array[i] + " ");
+        }
     }
 }
--- src/com/company/Painting.java
+++ src/com/company/Painting.java
@@ -2,13 +2,22 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class Painting extends Canvas {
 
+    private Color[] colors = {Color.MAGENTA, Color.GRAY, Color.BLUE};
+
     @Override
     public void paint(Graphics graphics) {
-        graphics.setColor(Color.MAGENTA);
-        graphics.fillOval(50,50, 100, 100);
+        final Circle[] circles = new Circle[7];
+        Random random = new Random();
+        for (int i = 0; i < circles.length; i++){
+            circles[i] = new Circle(random.nextInt(100), colors[random.nextInt(colors.length)], random.nextInt(500), random.nextInt(500));
+            graphics.setColor(circles[i].getColor());
+            graphics.fillOval(circles[i].getX(), circles[i].getY(), (int) circles[i].getRadius()*2, (int) circles[i].getRadius()*2);
+            System.out.printf("Area: %.2f, length: %.2f%n", circles[i].getCircleArea(),circles[i].getCircleLength());
+        }
     }
 
     public static void main(String[] args) {
