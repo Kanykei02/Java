cb2353a10a72ee9226959659d90449355bc58135
Kanykei02 <almazbekovakany@gmail.com>
2021-01-17 03:06:59 +0600

    Lesson32

--- src/com/company/Turtle.java
+++ src/com/company/Turtle.java
@@ -9,4 +9,7 @@ public class Turtle implements Swimmable{
     public void makeSound() {
         System.out.println("Turtle is hissing");
     }
+    public void layEggs(int eggsNumber) {
+        System.out.println("Turtle is laying " + eggsNumber + " eggs5541");
+    }
 }
