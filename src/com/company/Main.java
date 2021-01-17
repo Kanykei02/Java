bd235c2a934e9495a784790df6c5700976221e35
Kanykei02 <almazbekovakany@gmail.com>
2021-01-17 22:16:22 +0600

    Lesson33

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,21 @@
 package com.company;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileWriter fileWriter = new FileWriter("file1.txt");
+        for (char ch = 'A'; ch <= 'Z'; ch++){
+            fileWriter.write(ch);
+        }
+
+        fileWriter.write("\n");
+
+        for (int i = 0; i < 10; i++){
+            fileWriter.write(i + "");
+        }
+        fileWriter.close();
     }
 }
