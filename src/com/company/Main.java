f8d53fb9e514c325400175293ec48c2880721472
Kanykei02 <almazbekovakany@gmail.com>
2021-01-18 02:18:15 +0600

    Lesson33

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,9 @@
 package com.company;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -14,8 +16,33 @@ public class Main {
         fileWriter.write("\n");
 
         for (int i = 0; i < 10; i++){
-            fileWriter.write(i + "");
+            fileWriter.write(i + "\n");
         }
         fileWriter.close();
+
+        FileReader fileReader = new FileReader("file1.txt");
+        Scanner sc = new Scanner(fileReader);
+
+        FileWriter fileWriter1 = new FileWriter("file2.txt");
+
+        for (int i = 1; sc.hasNextLine(); i++){
+            String line = sc.nextLine();
+            if(i % 3 == 0) {
+                fileWriter1.write(line);
+                System.out.println(line);
+            }
+        }
+        fileReader.close();
+        fileWriter1.close();
+
+        try{
+            int[] array = {1, 2, 3};
+            System.out.println(array[3]);
+        }
+        catch (ArrayIndexOutOfBoundsException ex){
+            System.out.println("\n\nВы вышли за пределы массива!");
+            System.out.println(ex);
+            ex.printStackTrace();
+        }
     }
 }
