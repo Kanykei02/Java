2b879dc4491deb34327b9fda17037945f3cafdd6
Kanykei02 <almazbekovakany@gmail.com>
2021-01-17 13:28:54 +0600

    Lesson32

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,38 +1,52 @@
 package com.company;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        Swimmable[] oceanarrium = new Swimmable[15];
-        for (int i = 0; i < oceanarrium.length; i++){
-            if (i < 5) oceanarrium[i] = new Turtle();
-            else if (i < 10) oceanarrium[i] = new Shark();
-            else oceanarrium[i] = new Duck();
+    public static void main(String[] args) throws IOException {
+//        Swimmable[] oceanarrium = new Swimmable[15];
+//        for (int i = 0; i < oceanarrium.length; i++){
+//            if (i < 5) oceanarrium[i] = new Turtle();
+//            else if (i < 10) oceanarrium[i] = new Shark();
+//            else oceanarrium[i] = new Duck();
+//
+//            oceanarrium[i].makeSound();
+//        }
+//        System.out.println("****************************************************");
+//        for (int i = 0; i < oceanarrium.length; i++){
+//            if (oceanarrium[i] instanceof Shark)
+//                ((Shark)oceanarrium[i]).hunt();
+//            else if (oceanarrium[i] instanceof Turtle)
+//                ((Turtle)oceanarrium[i]).layEggs(10);
+//            System.out.println(oceanarrium[i].getClass());
+//        }
+//        Turtle[] turtles = new Turtle[5];
+//        Shark[] sharks = new Shark[5];
+//        Duck[] ducks = new Duck[5];
+//        int iTurtle = 0;
+//        int iShark = 0;
+//        int iDuck = 0;
+//
+//        for (Swimmable animal : oceanarrium){
+//            if (animal instanceof Turtle)
+//                turtles[iTurtle++] = (Turtle) animal;
+//            else if (animal instanceof Shark)
+//                sharks[iShark++] = (Shark) animal;
+//            else if (animal instanceof Duck)
+//                ducks[iDuck++] = (Duck) animal;
+//        }
+        FileWriter fileWriter = new FileWriter("file1.txt");
+        for (char ch = 'A'; ch <= 'Z'; ch++){
+            fileWriter.write(ch);
+        }
 
-            oceanarrium[i].makeSound();
-        }
-        System.out.println("****************************************************");
-        for (int i = 0; i < oceanarrium.length; i++){
-            if (oceanarrium[i] instanceof Shark)
-                ((Shark)oceanarrium[i]).hunt();
-            else if (oceanarrium[i] instanceof Turtle)
-                ((Turtle)oceanarrium[i]).layEggs(10);
-            System.out.println(oceanarrium[i].getClass());
-        }
-        Turtle[] turtles = new Turtle[5];
-        Shark[] sharks = new Shark[5];
-        Duck[] ducks = new Duck[5];
-        int iTurtle = 0;
-        int iShark = 0;
-        int iDuck = 0;
+        fileWriter.write("\n");
 
-        for (Swimmable animal : oceanarrium){
-            if (animal instanceof Turtle)
-                turtles[iTurtle++] = (Turtle) animal;
-            else if (animal instanceof Shark)
-                sharks[iShark++] = (Shark) animal;
-            else if (animal instanceof Duck)
-                ducks[iDuck++] = (Duck) animal;
+        for (int i = 0; i < 10; i++){
+            fileWriter.write(i + "");
         }
+        fileWriter.close();
     }
 }
