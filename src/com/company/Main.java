ab8c47cc1fdec8935a21f70113fcb21387d9cb02
Kanykei02 <almazbekovakany@gmail.com>
2021-01-23 23:18:47 +0600

    lesson35

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,15 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(20);
+        list.add(30);
+        list.add(10);
+        System.out.println(list.get(2));
+        System.out.println(list);
     }
 }
