e7b819ad5e936dd8ab4695b6fff09ff3a8e72a9d
Kanykei02 <almazbekovakany@gmail.com>
2021-01-27 23:51:55 +0600

    Zadacha 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,18 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        List<Integer> array = new ArrayList<>();
+        for (int i = 1; i <= 100; i++){
+            array.add(i);
+        }
+        System.out.println(array);
+        Set<Integer> hashSet = new HashSet<>(array);
+        for (Iterator<Integer> it = hashSet.iterator(); it.hasNext();){
+
+        }
     }
 }
