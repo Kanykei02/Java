eb97486abc461d87dabcef46906651c0273c315c
Kanykei02 <almazbekovakany@gmail.com>
2021-03-31 00:45:12 +0600

    сд

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,29 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        OldGenerics intGen = new OldGenerics(5);
+        OldGenerics strGen = new OldGenerics("string");
+
+        System.out.println(intGen);
+        System.out.println((int)intGen.getVal() + 2);
+        System.out.println(strGen);
+        System.out.println(strGen.getVal() + ", number");
+    }
+}
+
+class OldGenerics{
+    private Object val;
+
+    public OldGenerics(Object val){
+        this.val = val;
+    }
+
+    @Override
+    public String toString(){
+        return " (" + val + ") ";
+    }
+
+    public Object getVal() {
+        return val;
     }
 }
