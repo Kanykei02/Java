b7f4b9db31ce095d01084b6ca89345c1bc1a3fa9
Kanykei02 <almazbekovakany@gmail.com>
2021-04-07 19:02:07 +0600

    кр

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -31,6 +32,20 @@ public class Main {
         List<String> stringList = Arrays.asList("Alex", "Sam");
         stringList.forEach(x -> System.out.println(x.replace("a", "o")));
 
+        num.stream().map(x -> x * 2).map(x -> x - 1).forEach(x -> System.out.println(x));
 
+        List<Integer> procent = Arrays.asList(123, 1341234, 26736, 62356);
+        procent.stream().map(x -> x / 100).map(x -> x * 10).forEach(System.out::println);//можно просто умножить на 0,1)) ну ты клоун Каныкей
+
+        List<String> str = Arrays.asList("Olya", "Yulya");
+        str.stream().forEach(x -> System.out.println(x + x));
+
+        List<Student> students = Arrays.asList(new Student("Alex", 1));
+        students.stream().map(x -> x.getName()).forEach(x -> System.out.println(x));
+
+        List<String> stringList1 = students.stream().map(x -> x.getName()).collect(Collectors.toList());
+        System.out.println(stringList1);
+
+         
     }
 }
